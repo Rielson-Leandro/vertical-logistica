688cc424fc89c63d7c76d71b765303cddd065a58
Rielson-Leandro <rielson.leandro1@gmail.com>
2024-05-22 19:56:43 -0300

    first commit

---
+++ src/main/kotlin/br/com/vertical/logistica/VerticalLogisticaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.vertical.logistica
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VerticalLogisticaApplication
+
+fun main(args: Array<String>) {
+	runApplication<VerticalLogisticaApplication>(*args)
+}
