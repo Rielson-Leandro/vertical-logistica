23f2b0f680aa718408f1b0037f157b5734d9fc41
Rielson-Leandro <rielson.leandro1@gmail.com>
2024-05-29 13:42:57 -0300

    refact: refatoração

--- src/main/kotlin/br/com/vertical/logistica/config/CacheConfig.kt
+++ src/main/kotlin/br/com/vertical/logistica/config/CacheConfig.kt
@@ -5,19 +5,18 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
+import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.GenericToStringSerializer
-import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.GenericToStringSerializer
 import java.time.Duration
 
 @Configuration
 @ComponentScan("br.com.vertical.logistica")
 @EnableRedisRepositories(basePackages = ["br.com.vertical.logistica"])
 @PropertySource("classpath:application.properties")
-class RedisCacheConfig {
-
+class CacheConfig {
     @Bean
     fun jedisConnectionFactory(): JedisConnectionFactory {
         return JedisConnectionFactory()
@@ -40,7 +39,3 @@ class RedisCacheConfig {
         }
     }
 }
-
-
-
-
